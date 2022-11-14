cf4bcc87d8ca801f3d5cf076f18260591561644f
Amanjot Singh <62309381+reachajs@users.noreply.github.com>
2022-11-14 23:18:40 +0530

    initial commit

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Prime
+{
+            public static void main(String args[])
+            {
+                        int n,f;
+                        Scanner scr=new Scanner(System.in);
+                        System.out.println("\nEnter n value:  ");
+                        n=scr.nextInt();
+                        System.out.println("\nPrime numbers are : ");
+                        for(int i=2;i<=n;i++)
+                        {
+                                    f=0;
+                                    for(int j=2;j<=i/2;j++)
+                                    if((i%j)==0)
+                                    {
+                                                f=1;
+                                                break;
+                                    }
+                        if(f==0)
+                        System.out.print(i+"   ");
+                        }
+            }
+}
